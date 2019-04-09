8a33d42fd9d3e0250db4d7bd50761d4c6e4b4842
DG <d.gill.design@gmail.com>
2019-04-09 18:15:29 +0000

    Automatically backed up by Learn

--- flow-control.js
+++ flow-control.js
@@ -1,9 +1,10 @@
+
 function basicTeenager(age) {
-  if (age >12 && <20) {
+  if (age > 12 && age < 20) {
     return "You're a teen!"
   }
-basicTeenager(10)
 }
+basicTeenager(10)
 
 function teenager(age) {
 
