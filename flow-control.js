1dc5d63fb6a6f13a15d8d1ca0156155134cb268f
DG <d.gill.design@gmail.com>
2019-04-09 18:56:11 +0000

    Automatically backed up by Learn

--- flow-control.js
+++ flow-control.js
@@ -7,12 +7,24 @@ function basicTeenager(age) {
 basicTeenager(10)
 
 function teenager(age) {
-
+  if (age > 12 && age < 20) {
+    return "you're a teen!"
+  } else {
+    return "Not a teen!"
+  }
 }
+teenager(5)
 
 function ageChecker(age) {
-
+  if (age > 19) {
+    return "Sup gramps!"
+  } else if (age > 12 && age < 20) {
+    return "Sup teeny!" 
+  } else {
+    return "Sup youngin!"
+  }
 }
+ageChecker(12)
 
 function ternaryTeenager(age) {
 
